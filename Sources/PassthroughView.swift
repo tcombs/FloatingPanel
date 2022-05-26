0ffa5c2885b51af07897da533c5b4650114d42cd
Tyler Combs <tyler@tlcombs.com>
2022-05-26 10:59:09 -0500

    Update PassthroughView.swift

--- Sources/PassthroughView.swift
+++ Sources/PassthroughView.swift
@@ -6,12 +6,6 @@ import UIKit
 class PassthroughView: UIView {
     public weak var eventForwardingView: UIView?
     public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        let hitView = super.hitTest(point, with: event)
-        switch hitView {
-        case self:
-            return eventForwardingView?.hitTest(self.convert(point, to: eventForwardingView), with: event)
-        default:
-            return hitView
-        }
+        return super.hitTest(point, with: event)
     }
 }
